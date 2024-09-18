297fcea7f5dd1c18a962f78b40bee80c43e48a6e
juan manuel <jmgv87191@gmail.com>
2024-09-18 12:55:06 -0700

    asd

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule,} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule, MatButtonModule, MatDividerModule, MatIconModule,
+    MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, RouterLink
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule,} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
+  imports: [ MatFormFieldModule, MatButtonModule, MatDividerModule, MatIconModule,
+    MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, RouterLink
+  ],  templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {

e8c68d16a605d83c69f0c61f917c1b71feba112a
jmgv87191 <jmgv87191@gmail.com>
2024-09-17 17:09:08 -0700

    asd

--- src/app/heroes/heroes.routes.ts
+++ src/app/heroes/heroes.routes.ts
@@ -3,6 +3,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 export const routes: Routes = [
@@ -11,10 +12,7 @@ export const routes: Routes = [
         path:'',
         component: LayoutPageComponent
     },
-    {
-        path:'hero',
-        loadComponent: ()=> import ('./pages/hero-page/hero-page.component').then( m => m.HeroPageComponent )
-    },
+
     {
         path:'list',
         loadComponent: ()=> import ('./pages/list-page/list-page.component').then( m => m.ListPageComponent )
@@ -22,7 +20,21 @@ export const routes: Routes = [
     {
         path:'new',
         loadComponent: ()=> import('./pages/new-page/new-page.component').then(m => m.NewPageComponent)
+    },
+    {
+        path:'edit/:id',
+        loadComponent: ()=> import('./pages/new-page/new-page.component').then(m => m.NewPageComponent)
+    },
+    {
+        path:'search',
+        loadComponent: ()=> import('./pages/search-page/search-page.component').then(m => m.SearchPageComponent)
+    },
+    {
+        path:':id',
+        loadComponent: ()=> import ('./pages/hero-page/hero-page.component').then( m => m.HeroPageComponent )
+    },
+    {
+        path:'**', redirectTo:'list'
     }
-
 
 ];

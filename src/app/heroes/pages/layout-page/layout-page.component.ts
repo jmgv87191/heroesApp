0c8591262a148eef5d112eda2b29aeea30ad262f
juan manuel <jmgv87191@gmail.com>
2024-09-18 14:23:20 -0700

    asd

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatSidenavModule, MatSidenavContainer,} from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSidenavModule,CommonModule,MatToolbarModule,MatButtonModule, MatIconModule,
+    MatListModule,RouterLink
+  ],
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css'
 })
 export class LayoutPageComponent {
+
+
+  public sidebarItems = [
+    {
+      laber:'listado',
+      icon: 'label',
+      url:'./list'
+    },
+    {
+      laber:'Añadir',
+      icon: 'add',
+      url:'./new-hero'
+    },
+    {
+      laber:'Buscar',
+      icon: 'search',
+      url:'./search'
+    },
+  ]
 
 }

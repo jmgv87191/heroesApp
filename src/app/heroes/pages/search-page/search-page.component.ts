f3b21779fc21108cac21aa82fd93e1a3b0fa6431
jmgv87191 <jmgv87191@gmail.com>
2024-09-22 14:08:14 -0700

    asd

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,ReactiveFormsModule,
+CommonModule
+
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.css'
 })
 export class SearchPageComponent {
+  
+  public searchInput = new FormControl('');
 
 }
--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -26,4 +26,8 @@ export class HeroService {
     )
   }
 
+  getSuggestions(query:string):Observable<Hero[]>{
+    return this.http.get<Hero[]>(  `${ this.baseUrl }/heroes?q=${query}&_limit=6` )
+  }
+
 }

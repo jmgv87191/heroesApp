096d27a16edc2f91e3dc97650219cdc1d17e615f
juan manuel <jmgv87191@gmail.com>
2024-09-23 08:34:30 -0700

    asd

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -4,6 +4,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Hero } from '../../interfaces/hero';
 
 @Component({
   selector: 'app-search-page',
@@ -19,5 +20,10 @@ CommonModule
 export class SearchPageComponent {
   
   public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+
+  searcHero(){
+    const value: string = this.searchInput.value || '';
+  }
 
 }
